6820220fcba0ed8301fd2edffed70c2cd1ad3f6c
Sartaj Singh <ssingh.sartaj@gmail.com>
2019-07-09 16:54:40 -0700

    Scripts Added
    The added Python scripts will create JSON files as such:
    'schools_ids' - a collection of all source schools (CCs) with a json file of n elements with attributes 'id' and 'name'
    
    'agreement_ids' -  json file of n elements with atrributes 'id' (id# of source school) and 'agreementIds' (array of Id#s for destination schools

---
+++ cs160-project/python_webreader/get_schools_agreementids.py
@@ -0,0 +1,34 @@
+import json
+from urllib.request import urlopen # using the urllib built in module, we are importing urlopen
+from collections import namedtuple
+
+with urlopen("https://assist.org/api/institutions") as response:
+    source = response.read()
+    # setting source variable as response upon which read is being called, not very readable
+
+readable_json = json.loads(source)
+#readable_json is parsable object
+
+tempList = list()
+#iterates through every item in the json and appends to the dictionary
+for item in readable_json:
+    idNum = item['id']
+    #decalre tempList to add to json
+    print('Information being retrieved from school id:',idNum,"\n")
+    with urlopen("https://assist.org/api/institutions/"+str(idNum)+"/agreements") as response:
+        tempSchool = response.read()
+
+    tempSchool_json = json.loads(tempSchool)
+
+    tempArr = []
+    for school in tempSchool_json:
+        tempArr.append(school['institutionParentId'])
+
+    x = {
+       "id": idNum,
+       "agreementIds" : tempArr
+    }
+    tempList.append(x)
+
+with open('agreement_ids.json', 'w') as f:   
+    json.dump(tempList, f, indent = 2)
