2ba72e4615d3699601a16d52d86aab2259704287
Sartaj Singh <ssingh.sartaj@gmail.com>
2019-07-11 18:23:20 -0700

    Update get_majors.py
    minor text fixes

--- python_webreader/get_majors.py
+++ python_webreader/get_majors.py
@@ -9,7 +9,6 @@ with open('agreement_ids.json') as f:
         sourceId = x['id']
         tempList = list()
         for destId in x['agreementIds']:
-            #while(source['reports'])
             with urlopen("https://assist.org/api/agreements?receivingInstitutionId="+str(destId)+"&sendingInstitutionId="+str(sourceId)+"&academicYearId=67&categoryCode=major") as response:
                 source = response.read()
             readable_json = json.loads(source)
@@ -22,6 +21,3 @@ with open('agreement_ids.json') as f:
             if(tempList.__len__()>0):
                 with open(os.getcwd()+'/jsons/'+str(sourceId)+'_'+str(destId)+'.json', 'w') as f:   
                     json.dump(tempList, f, indent = 2)
-        
-        
-#https://assist.org/api/agreements?receivingInstitutionId=39&sendingInstitutionId=113&academicYearId=67&categoryCode=major
