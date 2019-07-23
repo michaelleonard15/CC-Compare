8645ea51f329a46c72587574a31a8252c51c34f4
Sartaj Singh <ssingh.sartaj@gmail.com>
2019-07-22 18:23:14 -0700

    Object Oriented Design of the Parsing Project

---
+++ textParsing/FileSearcher.py
@@ -0,0 +1,31 @@
+import os
+
+class ToolBelt:
+    fileList = []
+    def __init__(self,files):
+        self.fileList = files
+    def getMatches(self, pattern):
+        matches = []
+        for x in self.fileList:
+            with open(os.getcwd()+'/textfiles/'+x) as f:
+                contents = f.read()
+            matches.append(pattern.finditer(contents))
+            return matches
+
+    def printMatches(self, matches):
+        for match in matches:
+            for x in match:
+                    print(x.group(1))
+
+    def createMasterList(self, side):
+        temp = []
+        masterList = []
+        for match in side:
+            for found in match:
+                    if found.group(1) is not None:
+                                    temp.append(found.group(1))
+                    if found.group(1) is None:
+                            masterList.append(temp.copy())
+                            temp.clear()
+        return masterList
+                                
--- textParsing/parsing.py
+++ textParsing/parsing.py
@@ -1,6 +1,7 @@
 import os
 import re
-id_list = { 
+from FileSearcher import ToolBelt
+id_list = [
     '14782302.txt',
     # '14782303.txt',
     # '14782304.txt',
@@ -15,16 +16,25 @@ id_list = {
     # '14782313.txt',
     # '14782314.txt',
     # '14782315.txt',
-}
-pattern = re.compile(r'.*\|.*')
-matches = []
-i = 0
-for item in id_list:
-    with open(os.getcwd()+'/textfiles/'+item) as f:
-        i+=1
-        print('Successfully opened files: ', i)
-        contents = f.read()
-        matches.append(pattern.finditer(contents))
-for match in matches:
-    for found in match:
-        print(found)
+]
+tool = ToolBelt(id_list)
+#DEFINE PATTERNS HERE.####
+pattern_left = re.compile(r'(.*)\|.*|----+')
+pattern_right = re.compile(r'\|(.*)|----+')
+class_key = re.compile(r'[A-Z]{2-4}\ ')
+####
+
+#CREATE MATCHES FROM FILES HERE, PASS PATTERNS, GET MATCHES####
+
+matches_left = tool.getMatches(pattern_left)
+matches_right = tool.getMatches(pattern_right)
+####
+
+
+####PRINT MATCHES
+#tool.printMatches(matches_right)
+
+
+##CREATE LIST OF LISTS FOR SPECIFIED SIDE## 
+masterLeft = tool.createMasterList(matches_left)
+masterRight = tool.createMasterList(matches_right)
