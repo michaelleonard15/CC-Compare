6f1e1fa31f8f03cd8b9b798e62b7a52bd5c3194d
michaelleonard15 <47129066+michaelleonard15@users.noreply.github.com>
2019-07-23 10:02:47 -0700

    One change not included in last commit

--- cs160-project/src/RequirementRow.js
+++ cs160-project/src/RequirementRow.js
@@ -12,11 +12,7 @@ class RequirementRow extends React.Component {
     this.state = {completed: this.isCompleted()}
   }
 
-  /*
-    If you're reading this, I'm sorry.
-    This code is bad and I feel bad. 
-    Hopefully it's only temporary.
-  */
+
   isCompleted() {
     let IDs = this.props.requirements[0].IDs
     let relation = this.props.requirements[0].relation
@@ -32,31 +28,13 @@ class RequirementRow extends React.Component {
     
     // Convert IDs in expr to corresponding boolean values.
     IDs = IDs.map( (ID) => {return this.props.lookupTable.get(ID).isSelected} )
+    // Convert "AND", "OR" to "&&", "||"
     relation = relation.map( (bool) => {return bool === "OR"  ? "||" : "&&"}  )
     let expr = IDs[0]
     for(let i = 0; i < relation.length; i++) {
       expr += relation[i] + IDs[i +1]
     }
     return eval(expr)
-/*
-    // Handle any && logic first. Splice the two arrays to 
-    // remove the && and replace two bool values with the result.
-    for (let i = 0; i < relation.length; i++) {
-      if(relation[i] === 'AND') {
-        expr.splice(i, 2, (expr[i] && expr[i+1]) )
-        relation.splice(i, 1)
-      }
-    }
-
-    // Handle any || operations next.
-    let bool = false
-    for(let i = 0; i < relation.length; i++) {
-      if(relation[i] === 'OR') {
-        bool = bool || expr[i] || expr[i+1]
-      }
-    }
-    return bool
-    */
   }
 
 
