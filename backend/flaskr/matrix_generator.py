24eec422a7803e26b08a817d06a82423161c079f
mdemong <mdemong1@gmail.com>
2019-07-29 19:16:18 -0700

    Wrote method stubs

---
+++ backend/flaskr/matrix_generator.py
@@ -0,0 +1,23 @@
+
+
+class MatrixGenerator:
+    
+
+    def __init__(self):
+        self.lookup = []
+        self.matrix = [[]]
+
+
+    def add_agreement(self, agreement):
+        pass
+    
+
+    # Generates and returns an object containing:
+    #   - the class lookup table
+    #   - the equivalency matrix
+    def generate_object():
+        pass
+
+
+    def get_object():
+        pass
