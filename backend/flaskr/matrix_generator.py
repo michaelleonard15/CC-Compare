0bc6cfe251fd45b91f0a48f532fcc13d9768615f
mdemong <mdemong1@gmail.com>
2019-07-30 00:27:49 -0700

    still refining matrix-generator skeleton

--- backend/flaskr/matrix_generator.py
+++ backend/flaskr/matrix_generator.py
@@ -4,20 +4,56 @@ class MatrixGenerator:
     
 
     def __init__(self):
-        self.lookup = []
+
+        self.source_lookup = []
+        self.dest_lookup = []
         self.matrix = [[]]
+        self.agreements = []
+        self.is_generated = false
 
 
     def add_agreement(self, agreement):
+
         pass
     
 
     # Generates and returns an object containing:
     #   - the class lookup table
     #   - the equivalency matrix
-    def generate_object():
-        pass
+    def get(self):
+        if not self.is_generated:
+            self.generate
+        
+
+    
+    def generate(self):
+        _extract_all_courses
+
+    def _extract_all_courses(self):
+
+        for agreement in self.agreements:
+            _extract_courses(self, agreement)
 
 
-    def get_object():
-        pass
+    def _extract_courses(self, agreement):
+        
+        for section in agreement:
+            # Ignoring section headers for now, since there's nothing there yet
+            for row in section:
+                _extract_row(self, row)
+
+
+    def _extract_row(self, row):
+        
+
+
+
+
+
+### NOTES
+# relationToNext ==> relationToNext 
+    # key ==> courseID
+    # name ==> courseName
+    # units (string) ==> units (integer)
+    # after replacing objects with numbers, array structure should be IDENTICAL
+# 
