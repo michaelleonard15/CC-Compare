046735fc0ed72351f756e3fa11c776c87384fcc5
mdemong <mdemong1@gmail.com>
2019-07-30 19:56:49 -0700

    modified function args and re-ordered them

--- backend/flaskr/matrix_generator.py
+++ backend/flaskr/matrix_generator.py
@@ -1,6 +1,5 @@
 # The documentation in this file uses Python docstrings.
 # See https://www.python.org/dev/peps/pep-0257/ for more info
-# Docstring
 
 
 def generate_matrix(agreement_list):
@@ -9,49 +8,32 @@ def generate_matrix(agreement_list):
     and sending to the frontend.
     
     Args:
-        agreement_list: A list containing 4 agreement maps (which were converted from JSON)
+        agreement_list: A list containing 4 agreement maps (which were converted from JSON).
+                        This argument will be mutated by this function.
     """
     source_lookup = []
     dest_lookup = []
     matrix = [[]]
     for agreement in agreement_list:
         # Ignoring section headers for now, since there's nothing there yet
-        current_source_lookup = _extract_sources(agreement, matrix)
+        _extract_sources(agreement, matrix, source_lookup)
         current_dest_lookup = _extract_dest(agreement, matrix)
         
 
-pr
 
-
-def _extract_sources(matrix):
+def _extract_sources(agreement, matrix, source_lookup):
     """
-    Extracts all sources from an agreement, building onto the existing matrix.
-    Returns a source lookup table for this agreement.
+    Extracts all sources from an agreement, 
+    building onto the existing matrix and source lookup table.
     """
-    cur_src_lookup = []
 
     for section in agreement:
         for row in section:
-            _extract_source_row(agreement, matrix, cur_src_lookup, row)
+            _extract_source_row(row, matrix, source_lookup)
     return cur_src_lookup
 
 
-
-def _extract_dests(matrix):
-    """
-    Extracts all destinations from an agreement, building onto the existing matrix.
-    Returns a destination lookup table for this agreement.
-    """
-    cur_dest_lookup = []
-    for section in agreement:
-        for row in section:
-            _extract_dest_row(agreement, matrix, cur_dest_lookup, row)
-    
-    _fill_empty_dests(matrix)
-    return cur_dest_lookup
-
-
-def _extract_source_row(agreement, matrix, cur_src_lookup, row):
+def _extract_source_row(row, matrix, src_lookup):
     """
     Extracts source schools from a row, building onto the existing matrix and the
     source lookup table for the current agreement.
@@ -60,13 +42,27 @@ def _extract_source_row(agreement, matrix, cur_src_lookup, row):
     pass
 
 
-def _extract_dest_row(agreement, matrix, cur_dest_lookup, row):
+def _extract_dests(matrix):
+    """
+    Extracts all destinations from an agreement, building onto the existing matrix.
+    Returns a new destination lookup table for this agreement.
+    """
+    cur_dest_lookup = []
+    for section in agreement:
+        for row in section:
+            _extract_dest_row(row, matrix, cur_dest_lookup)
+    
+    _fill_empty_dests(matrix)
+    return cur_dest_lookup
+
+
+def _extract_dest_row(row, matrix, cur_dest_lookup):
     """
     Extracts destination classes from a row, building onto the existing matrix and the 
     source lookup table for the current agreement.
     """
-    pass
 
+    pass
 
 
 def _fill_empty_dests(matrix):
@@ -74,6 +70,7 @@ def _fill_empty_dests(matrix):
     Goes through the matrix and adds empty destination classes wherever there is
     a cell missing in the column.
     """
+
     pass
 
 
