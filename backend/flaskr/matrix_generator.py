8dea17d90f8b5192e6ee675e9a5cf0936f41df23
mdemong <mdemong1@gmail.com>
2019-07-30 18:59:08 -0700

    Modified matrx_generator to use procedural rather than OO structure

--- backend/flaskr/matrix_generator.py
+++ backend/flaskr/matrix_generator.py
@@ -1,53 +1,80 @@
+# The documentation in this file uses Python docstrings.
+# See https://www.python.org/dev/peps/pep-0257/ for more info
+# Docstring
 
 
-class MatrixGenerator:
+def generate_matrix(agreement_list):
+    """
+    Generates a map containing a lookup table and matrix, ready for conversion to JSON
+    and sending to the frontend.
     
-
-    def __init__(self):
-
-        self.source_lookup = []
-        self.dest_lookup = []
-        self.matrix = [[]]
-        self.agreements = []
-        self.is_generated = false
-
-
-    def add_agreement(self, agreement):
-
-        pass
-    
-
-    # Generates and returns an object containing:
-    #   - the class lookup table
-    #   - the equivalency matrix
-    def get(self):
-        if not self.is_generated:
-            self.generate
+    Args:
+        agreement_list: A list containing 4 agreement maps (which were converted from JSON)
+    """
+    source_lookup = []
+    dest_lookup = []
+    matrix = [[]]
+    for agreement in agreement_list:
+        # Ignoring section headers for now, since there's nothing there yet
+        current_source_lookup = _extract_sources(agreement, matrix)
+        current_dest_lookup = _extract_dest(agreement, matrix)
         
 
+pr
+
+
+def _extract_sources(matrix):
+    """
+    Extracts all sources from an agreement, building onto the existing matrix.
+    Returns a source lookup table for this agreement.
+    """
+    cur_src_lookup = []
+
+    for section in agreement:
+        for row in section:
+            _extract_source_row(agreement, matrix, cur_src_lookup, row)
+    return cur_src_lookup
+
+
+
+def _extract_dests(matrix):
+    """
+    Extracts all destinations from an agreement, building onto the existing matrix.
+    Returns a destination lookup table for this agreement.
+    """
+    cur_dest_lookup = []
+    for section in agreement:
+        for row in section:
+            _extract_dest_row(agreement, matrix, cur_dest_lookup, row)
     
-    def generate(self):
-        _extract_all_courses
-
-    def _extract_all_courses(self):
-
-        for agreement in self.agreements:
-            _extract_courses(self, agreement)
+    _fill_empty_dests(matrix)
+    return cur_dest_lookup
 
 
-    def _extract_courses(self, agreement):
-        
-        for section in agreement:
-            # Ignoring section headers for now, since there's nothing there yet
-            for row in section:
-                _extract_row(self, row)
+def _extract_source_row(agreement, matrix, cur_src_lookup, row):
+    """
+    Extracts source schools from a row, building onto the existing matrix and the
+    source lookup table for the current agreement.
+    """
+
+    pass
 
 
-    def _extract_row(self, row):
-        
+def _extract_dest_row(agreement, matrix, cur_dest_lookup, row):
+    """
+    Extracts destination classes from a row, building onto the existing matrix and the 
+    source lookup table for the current agreement.
+    """
+    pass
 
 
 
+def _fill_empty_dests(matrix):
+    """
+    Goes through the matrix and adds empty destination classes wherever there is
+    a cell missing in the column.
+    """
+    pass
 
 
 ### NOTES
